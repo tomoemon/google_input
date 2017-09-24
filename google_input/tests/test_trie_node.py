2d1ecae9ee0f9769619d1bdcf558106126f5c398
tomoemon <bach48+github_tomoemon@gmail.com>
2017-09-25 00:07:48 +0900

    テスト追加

---
+++ google_input/tests/test_trie_node.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from pprint import pprint
+from google_input.ime import GoogleInputIME, TrieNode
+from google_input.filter_rule import FilterRuleTable, FilterRule
+from google_input import data
+
+
+def test_simple_rule():
+    rules = [
+        FilterRule("abc", "ABC", ""),
+    ]
+
+    root = TrieNode()
+    for rule in rules:
+        TrieNode.make(root, rule, rule.input)
+
+    c = root
+    assert sorted(c.keys()) == ["a"]
+    assert c.buffer == ""
+    assert c.output_rule is None
+    c = c["a"]
+    assert sorted(c.keys()) == ["b"]
+    assert c.buffer == "a"
+    assert c.output_rule is None
+    c = c["b"]
+    assert sorted(c.keys()) == ["c"]
+    assert c.buffer == "ab"
+    assert c.output_rule is None
+    c = c["c"]
+    assert sorted(c.keys()) == []
+    assert c.buffer == ""
+    assert c.output_rule == rules[0]
+
+
+def test_multi_rule():
+    rules = [
+        FilterRule("abx", "ABX", ""),
+        FilterRule("aby", "ABY", ""),
+    ]
+
+    root = TrieNode()
+    for rule in rules:
+        TrieNode.make(root, rule, rule.input)
+
+    c = root
+    assert sorted(c.keys()) == ["a"]
+    assert c.buffer == ""
+    assert c.output_rule is None
+    c = c["a"]
+    assert sorted(c.keys()) == ["b"]
+    assert c.buffer == "a"
+    assert c.output_rule is None
+    c = c["b"]
+    assert sorted(c.keys()) == ["x", "y"]
+    assert c.buffer == "ab"
+    assert c.output_rule is None
+    x = c["x"]
+    assert sorted(x.keys()) == []
+    assert x.buffer == ""
+    assert x.output_rule == rules[0]
+    y = c['y']
+    assert sorted(y.keys()) == []
+    assert y.buffer == ""
+    assert y.output_rule == rules[1]
